67a888106857175fd7af5275367759173e6f9cf2
juanmartinezdb <juanmmcontacto@gmail.com>
2025-11-22 09:22:18 +0100

    reset

--- frontend/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import { RouterView } from 'vue-router'
-</script>
-
-<template>
-  <div class="min-h-screen bg-gray-900 text-gray-100 font-sans">
-    <RouterView />
-  </div>
-</template>
